41892129d2188f1f48490d54483f58e132a7ffa6
Ravindu Dilsara <ravindudilsara@gmail.com>
2024-12-29 06:41:07 +0530

    Initial commit

---
+++ app/src/main/java/com/project/whatsappclone/Details.kt
@@ -0,0 +1,3 @@
+package com.project.whatsappclone
+
+data class Details(val image: Int,val name: String, val description: String)
---
+++ app/src/main/java/com/project/whatsappclone/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.project.whatsappclone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
